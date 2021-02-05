22b7f00aa682d802bc5222d24f31f0ef9125f94f
apanisile <apanisileifeoluwa@gmail.com>
2021-02-05 19:40:25 +0100

    added multiple functions

--- bash-scripting/install.sh
+++ bash-scripting/install.sh
@@ -1,72 +1,102 @@
-#!bin/bash
+#!/bin/bash
 
+while true
+do
+    echo "Hey there! what would you like to do today? "
+    echo "1. Install a software"
+    echo "2. Check if a software is enabled:"
+    echo "3. Check if a software is running: "
+    read -p "The selected option is: " choice
 
-read -p "Do you want to update the system repo? (Y)es or (N)o: " update
-if [ $update = "Y" ]
+    if [ $choice -eq 1 ]
     then 
-        #update the system repo
-        sudo apt update
-else
-    echo "Skipping to the installation process: "
-fi
-
-
-#what service is to be run?
-read -p "What service would you like to check for: " software
-
-#assigning the service to a variable
-l=$software
-
-echo "The service you are checking for is: ${l}"
-
-#Install the package
-sudo apt install $l -y
-
-
-#function to check if the service is enabled
-check_service_enabled() {
-	
-	service_enabled=$(sudo systemctl is-enabled $l)
-	
-	if [ $service_enabled = "enabled" ]
-		then
-			echo "$l is enabled"
-	else
-		echo "$l is not enabled"
-        read -p "Do you want to enable it? (Y)es or (N)o" answer
-        if [ $answer = "Y"]
+        read -p "Do you want to update the system repo? (Y)es or (N)o: " update
+        if [ $update = "Y" ]
             then 
-                sudo systemctl enable $l
+                #update the system repo
+                sudo apt update
         else
-            echo "Okay Chief"
+            echo "Skipping to the installation process: "
         fi
 
-        exit 1
+        #what software to be installed?
+        read -p "What software would you like to install: " software
+        
+        #assigning the service to a variable
+        l=$software
+        
+        echo "Declaration: You are about to install ${l} on your system"
+        
+        #Install the package
+        sudo apt install ${l} -y
 
-	fi
-}
+        sudo systemctl enable ${l}
 
-#function to check if the service is active
-check_service_active() {	
-	service_is_activ=$(sudo systemctl is-active $l)
+        sudo systemctl start ${l}
 
-	if [ $service_is_activ = "active" ]
-		then
-			echo "$l is active"
-	else 
-		echo "$l is not active"
-		exit 1
+    elif [ $choice -eq 2 ]
+    then
+        #what software to be installed?
+        read -p "What service would you like to enable: " software
+        
+        #assigning the service to a variable
+        l=$software
 
-	fi
-}
+        #function to check if the service is enabled
+        check_service_enabled() {
+            
+            service_enabled=$(sudo systemctl is-enabled $l)
+            
+            if [ $service_enabled = "enabled" ]
+                then
+                    echo "$l is enabled"
+            else
+                echo "$l is not enabled"
+                echo "Do you want to enable it? "
+                read -p "(Y)es or (N)o" answer
+                if [ $answer = "Y"]
+                    then 
+                        sudo systemctl enable $l
+                else
+                    echo "Okay Chief"
+                    break
+                fi
 
-#start the downloaded software
-systemctl start l
+                exit 1
+            fi
+        }
+        #check if the software is enabled
+        check_service_enabled l
 
-#check if the software is enabled
-check_service_enabled l
+    elif [ $choice -eq 3 ]
+    then
+        #prompt to activate a service
+        read -p "What service would you like to activate: " software
+        
+        #assigning the service to a variable
+        l=$software
 
-#Check if the software is active and running
-check_service_active l
+        #function to check if the service is active
+        check_service_active() {	
+            service_is_activ=$(sudo systemctl is-active $l)
 
+            if [ $service_is_activ = "active" ]
+                then
+                    echo "$l is active"
+            else 
+                echo "$l is not active"
+                break
+            fi
+        }
+        
+        #Check if the software is active and running
+        check_service_active l
+    fi  
 
+    echo "Do you want to perform another function? "
+    read -p "(Y)es or (N)o: " again
+    if [ $again != "Y" ]
+        then
+            exit 1
+        fi
+done
