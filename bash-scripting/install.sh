9645f57e70677bb83ca8724327504a63fe4ada5d
apanisile <apanisileifeoluwa@gmail.com>
2021-02-05 16:21:42 +0100

    restructuring and adding minor fixes

--- bash-scripting/install.sh
+++ bash-scripting/install.sh
@@ -1,17 +1,27 @@
 #!bin/bash
 
-#update the system repo
-sudo apt update
+
+read -p "Do you want to update the system repo? (Y)es or (N)o: " update
+if [ $update = "Y" ]
+    then 
+        #update the system repo
+        sudo apt update
+else
+    echo "Skipping to the installation process: "
+fi
+
 
 #what service is to be run?
 read -p "What service would you like to check for: " software
 
 #assigning the service to a variable
-l=$service
+l=$software
 
 echo "The service you are checking for is: ${l}"
 
+#Install the package
 sudo apt install $l -y
+
 
 #function to check if the service is enabled
 check_service_enabled() {
@@ -23,15 +33,21 @@ check_service_enabled() {
 			echo "$l is enabled"
 	else
 		echo "$l is not enabled"
-		exit 1
+        read -p "Do you want to enable it? (Y)es or (N)o" answer
+        if [ $answer = "Y"]
+            then 
+                sudo systemctl enable $l
+        else
+            echo "Okay Chief"
+        fi
+
+        exit 1
 
 	fi
 }
 
 #function to check if the service is active
-check_service_active() {
-	echo "The service you are checking for is: ${l}"
-	
+check_service_active() {	
 	service_is_activ=$(sudo systemctl is-active $l)
 
 	if [ $service_is_activ = "active" ]
@@ -44,9 +60,13 @@ check_service_active() {
 	fi
 }
 
-
+#start the downloaded software
 systemctl start l
+
+#check if the software is enabled
 check_service_enabled l
+
+#Check if the software is active and running
 check_service_active l
 
 
