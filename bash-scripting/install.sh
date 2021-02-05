a1d7f5ff668cce1f3714d89c268b9ab884dae3d4
apanisile <apanisileifeoluwa@gmail.com>
2021-02-05 14:59:23 +0100

    Modified the install file

--- bash-scripting/install.sh
+++ bash-scripting/install.sh
@@ -1,6 +1,52 @@
 #!bin/bash
+
+#update the system repo
 sudo apt update
-sudo apt install $1 -y
-systemctl enable $1
-systemctl start $1
+
+#what service is to be run?
+read -p "What service would you like to check for: " software
+
+#assigning the service to a variable
+l=$service
+
+echo "The service you are checking for is: ${l}"
+
+sudo apt install $l -y
+
+#function to check if the service is enabled
+check_service_enabled() {
+	
+	service_enabled=$(sudo systemctl is-enabled $l)
+	
+	if [ $service_enabled = "enabled" ]
+		then
+			echo "$l is enabled"
+	else
+		echo "$l is not enabled"
+		exit 1
+
+	fi
+}
+
+#function to check if the service is active
+check_service_active() {
+	echo "The service you are checking for is: ${l}"
+	
+	service_is_activ=$(sudo systemctl is-active $l)
+
+	if [ $service_is_activ = "active" ]
+		then
+			echo "$l is active"
+	else 
+		echo "$l is not active"
+		exit 1
+
+	fi
+}
+
+
+systemctl start l
+check_service_enabled l
+check_service_active l
+
 
